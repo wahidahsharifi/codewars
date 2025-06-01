5685c8e4d5200871aa46df4003a7937268b14169
Wahid Sharifi <119415805+wahidahsharifi@users.noreply.github.com>
2025-06-01 21:30:05 +0430

    Update solution - CodeHub

--- 6-kyu/help-the-bookseller/help-the-bookseller.js
+++ 6-kyu/help-the-bookseller/help-the-bookseller.js
@@ -1,7 +1,6 @@
 function stockList(books, categories) {
   let obj = {...categories}
-  const book = books.filter(e => categories.includes(e.slice(0,1)))
-  const booky = book.map(e => categories.includes(e.slice(0,1)) && obj[e.slice(0,1)] == undefined ? obj[e.slice(0,1)] = Number(e.match(/\d+/g)) : obj[e.slice(0,1)] += Number(e.match(/\d+/g)))
+  const book = books.map(e => categories.includes(e.slice(0,1)) && obj[e.slice(0,1)] == undefined ? obj[e.slice(0,1)] = Number(e.match(/\d+/g)) : obj[e.slice(0,1)] += Number(e.match(/\d+/g)))
   const result = `${categories.map(e => `(${e} : ${obj[e] == undefined ? 0 : obj[e]}) - `).join('').slice(0, -3)}`
   return books.length == 0 ? '' : result
 }
